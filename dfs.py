d5e739e0dfc35a03a0ae3d4fad47fe1346e79736
Garvit Verma <garvitjee@gmail.com>
2021-08-02 14:27:17 +0530

    removed dead code

--- dfs.py
+++ dfs.py
@@ -20,16 +20,3 @@ def depth_first_search(start_node):
       if not n.visited:
         stack.append(n)
 
-if __name__ == '__main__':
-  node1 = Node('A')
-  node2 = Node('B')
-  node3 = Node('C')
-  node4 = Node('D')
-  node5 = Node('E')
-
-  node1.adjacency_list.append(node2)
-  node1.adjacency_list.append(node3)
-  node2.adjacency_list.append(node4)
-  node4.adjacency_list.append(node5)
-
-  depth_first_search(node1)
